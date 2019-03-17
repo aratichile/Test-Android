e71f46433c6af55165ec0523138ee213b45c32a0
shishirYadav <shishir569@gmail.com>
2019-03-17 19:39:55 +0530

    changes from shishir

--- PatternTrangle.java
+++ PatternTrangle.java
@@ -1,50 +1,21 @@
 public class PatternTrangle {
 
     public static void main(String[] args) {
+        int n = 7;
+        for (int i = 0; i < n; i++) {
 
-        for (int i = 1; i <= 3; i++) {
-
-            for (int j = 3; j >= i; j--) {
+            for (int j = n - i; j > 1; j--) {
                 System.out.print(" ");
             }
-            for(int k=1;k<(i*2);k++){
-                System.out.print("*");
+
+            for (int j = 0; j <= i; j++) {
+                // printing stars
+                System.out.print("* ");
             }
+
+            // ending line after each row
             System.out.println();
         }
 
-        for (int i = 1; i <= 7; i++) {
-
-            for (int j = 3; j >= i; j--) {
-                System.out.print(" ");
-            }
-            for(int k=1;k<i;k++){
-                System.out.print(" *");
-            }
-            for(int l=1;l<=i;l++){
-                System.out.print(" *");
-            }
-            System.out.println();
-        }
-
-        for (int i = 1; i <= 4; i++) {
-
-            for (int j = 4; j >= i; j--) {
-                System.out.print(" ");
-            }
-            for(int k=1;k<i;k++){
-                System.out.print(" *");
-            }
-            System.out.println();
-        }for (int i = 5; i <= 7; i++) {
-
-            for (int j = 5; j >= i; j--) {
-                System.out.print(" ");
-            }
-            for(int k=1;k<i;k++){
-                System.out.print(" *");
-            }
-            System.out.println();
-        }
     }
 }
