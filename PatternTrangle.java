2dc251221a55926d73f6c1594f97a7f14bab47a3
CHILE AARTI <CHILAA-CONT@mahindra.com>
2019-03-17 20:18:09 +0530

    Added One More Pattern

--- PatternTrangle.java
+++ PatternTrangle.java
@@ -2,6 +2,17 @@ public class PatternTrangle {
 
     public static void main(String[] args) {
         int n = 7;
+
+	for (int i = 1; i <= n; i++) {
+
+            for (int j = n; j >= i; j--) {
+                System.out.print(" ");
+            }
+            for(int k=1;k<=i;k++){
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
         for (int i = 0; i < n; i++) {
 
             for (int j = n - i; j > 1; j--) {
